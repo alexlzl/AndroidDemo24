f57c072f0d814732f952bf9fc7b6bc0816c5d93c
liuzhouliang <525688067@qq.com>
2019-08-14 11:53:49 +0800

    init

--- app/src/main/java/com/example/liuzhouliang/androiddemo15/MainActivity.java
+++ app/src/main/java/com/example/liuzhouliang/androiddemo15/MainActivity.java
@@ -2,7 +2,17 @@ package com.example.liuzhouliang.androiddemo15;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+/**
+ * @ describe 分支测试
+ *
+ * @author lzl
+ *
+ * @ time 2019/8/14 11:53
+ *
+ * @ param
+ *
+ * @ return
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
