f2c461a4b8802224076a69cc6977efa01a48d00f
liuzhouliang <525688067@qq.com>
2019-08-14 11:57:11 +0800

    主干提交

--- app/src/main/java/com/example/liuzhouliang/androiddemo15/Main2Activity.java
+++ app/src/main/java/com/example/liuzhouliang/androiddemo15/Main2Activity.java
@@ -2,7 +2,17 @@ package com.example.liuzhouliang.androiddemo15;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+/**
+ * @ describe 主干提交
+ *
+ * @author lzl
+ *
+ * @ time 2019/8/14 11:56
+ *
+ * @ param
+ *
+ * @ return
+ */
 public class Main2Activity extends AppCompatActivity {
 
     @Override
